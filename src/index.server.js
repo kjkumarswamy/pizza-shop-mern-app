9c3bcfc144ca07ca8799d8923d9a2834e9a07314
kjkumarswamy <kumarhpt55@gmail.com>
2021-12-17 18:51:19 +0530

    update
    index updated

--- src/index.server.js
+++ src/index.server.js
@@ -6,14 +6,22 @@ const path = require("path");
 const morgon = require("morgan");
 const cors = require("cors");
 
+const PORT = process.env.PORT;
+connectDb();
+
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const pizzaRoutes = require("./routes/pizzas");
 const orderRoutes = require("./routes/order");
 
-connectDb();
+app.use(cors());
+app.use(express.json());
+app.use(morgon("dev"));
 
-const PORT = process.env.PORT;
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", pizzaRoutes);
+app.use("/api", orderRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
@@ -25,23 +33,6 @@ if (process.env.NODE_ENV === "production") {
     res.send("Hi this si working");
   });
 }
-
-app.get("/userdata", (req, res) => {
-  res.send("Hi this si working");
-});
-
-app.use(cors());
-app.use(express.json());
-app.use(morgon("dev"));
-
-app.use("/api", authRoutes);
-app.use("/api", userRoutes);
-app.use("/api", pizzaRoutes);
-app.use("/api", orderRoutes);
-
-app.get("/", (req, res) => {
-  res.send("hi this is working");
-});
 
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
